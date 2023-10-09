4a371243a08982033616f61f84901583b21eef7e
itpagautierfranchini <146729884+itpagautierfranchini@users.noreply.github.com>
2023-10-09 14:29:21 +0200

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
-
 # pylint: enable=line-too-long
-
-print("hello truc much :)")
-
 
 import pytest
 
+print("hello truc much :)")
 
 def f():
     raise SystemExit(1)
